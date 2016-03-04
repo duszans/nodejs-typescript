80b20281754831ecef4163f00bab6640c43d714f
duszans <duszans@gmail.com>
2016-03-04 22:12:37 +0100

    Added examples/info about proper importing a node_module.

--- lib/app.ts
+++ lib/app.ts
@@ -6,9 +6,26 @@ import 'source-map-support/register';
 // relative path thanks to symlink(for runtime) and moduleReferencing: classic (for tsc/IDE)
 import foo from 'lib/foo/foo';
 
+// to import whole module from node_modules use "import * as ... from '...'" - it's transpiled to "var ... = require('...');
+// importing default export via: import ... from '...' won't work because then '.default' is applied to all usages
+// of imported node_module. Otherwise "import { default as ... } from '...'" should be also working (if d.ts files
+// are compatibile to do so); Importing node_module which has named exports can be imported with
+// "import { namedExport1, namedExport2 } from '...'" etc.
+import * as chalk from 'chalk';
+console.log(chalk.bold.red('chalked'));
+
+// works even with functions as default exports
+import * as moment from 'moment';
+console.log(moment());
+
+// example why default import won't work:
+// second of this two lines would result in a "TypeError: moment_1.default is not a function"
+// import moment1 from 'moment';
+// console.log(moment1());
+
 foo();
 
-// no cycle reference even when using ambient declarations placed inside linked dir
+// no cycle reference even when using ambient declarations placed inside symlinked dir
 class World implements ICanHello {
     constructor () {
         console.log('constructor');
